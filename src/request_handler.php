2fe1e3df6819290c3b1b8d8d2a8e8c96e9aaf87e
Vgabor33 <veressgabi33@gmail.com>
2021-11-15 14:34:01 +0100

    request_handler tokengeneráló osztály hozzáadva a generáló metódusokkal együtt

--- src/request_handler.php
+++ src/request_handler.php
@@ -1,6 +1,8 @@
 <?php
 header("Content-Type: text/json; charset=UTF-8");
 
+$action = $_GET['action'];
+
 function checkCookies(): bool
 {
     return isset($_COOKIE['client-token']) && isset($_COOKIE['server-token']);
@@ -11,4 +13,20 @@ function badCookies(): void
     http_response_code(400);
     echo '"Please register!"';
 }
+
+class TokenGenerator
+{
+    public static function get_serverToken()
+    {
+        return random_int(1000000000000000,9999999999999999);
+    }
+
+    public static function get_userToken()
+    {
+        return random_int(1000000000000000,9999999999999999);
+    }
+}
+
+
+
 ?>
