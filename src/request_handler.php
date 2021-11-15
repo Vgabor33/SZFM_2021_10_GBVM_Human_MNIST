4f27b1b1cfb6440aee1501c73c3f4f795c617a0e
Vgabor33 <veressgabi33@gmail.com>
2021-11-15 14:30:02 +0100

    request_handler.php cookie ellenörzés kibővítve a hibakezeléssel

--- src/request_handler.php
+++ src/request_handler.php
@@ -5,4 +5,10 @@ function checkCookies(): bool
 {
     return isset($_COOKIE['client-token']) && isset($_COOKIE['server-token']);
 }
+
+function badCookies(): void
+{
+    http_response_code(400);
+    echo '"Please register!"';
+}
 ?>
